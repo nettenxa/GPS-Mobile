1430f3f4725d364fa6ef764da59dc1f5cf12a038
nettenxa <atibodee.mah@dome.tu.ac.th>
2021-11-03 20:09:41 +0700

    GPS

--- app/src/main/java/com/example/gpsteackking/MainActivity.java
+++ app/src/main/java/com/example/gpsteackking/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.gpsteackking;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
-import android.location.LocationRequest;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Switch;
 import android.widget.TextView;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
     public static  final  int DEFAULT_UPDATE_INTERVAL = 30;
@@ -40,6 +47,37 @@ public class MainActivity extends AppCompatActivity {
         locationRequest.setInterval(1000 * DEFAULT_UPDATE_INTERVAL);
         locationRequest.setFastestInterval(1000 * FAST_UPDATE_INTERVAL);
 
-        locationRequest.setPriority(LocationRequest.PRIORITY_BALACED_POWER_ACCURACY);
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        sw_gps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(sw_gps.isChecked()) {
+                    locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                    tv_sensor.setText("Using GPS Sensor");
+                }
+                else {
+                    locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+                    tv_sensor.setText("Using Wower + WIFI");
+                }
+            }
+
+            private void updateGPS(){
+                fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this)
+                if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    // User provided the permission
+                    fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+
+                        }
+                    });
+                }
+                else {
+                    // Permiss not grated yet
+                }
+            }
+
+        });
     }
 }
