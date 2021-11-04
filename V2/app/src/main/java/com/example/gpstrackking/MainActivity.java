1ebfb3aa3865825a81bf0ac7fa66150a1d1fc6af
nettenxa <atibodee.mah@dome.tu.ac.th>
2021-11-04 08:59:43 +0700

    a

--- V2/app/src/main/java/com/example/gpstrackking/MainActivity.java
+++ V2/app/src/main/java/com/example/gpstrackking/MainActivity.java
@@ -7,10 +7,14 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.app.Service;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
@@ -18,28 +22,33 @@ import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements LocationListener {
     private static final int PREMISSION_CODE = 101;
     TextView locationText;
     Button getLocation;
     String[] permissions_all = {Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
     LocationManager locationManager;
+    Location loc;
     boolean isGpsLocation;
     boolean isNetworkLocation;
-
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        progressDialog= new ProgressDialog(MainActivity.this);
+        progressDialog.setMessage("Fecthing location...");
+
         locationText = findViewById(R.id.location);
         getLocation = findViewById(R.id.getlocation);
 
         getLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                progressDialog.show();
                 getLocation();
             }
         });
@@ -58,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
             getDeviceLocation();
         }
     }
+
     private void reqestPermission() {
         ActivityCompat.requestPermissions(MainActivity.this,permissions_all,PREMISSION_CODE);
     }
@@ -76,14 +86,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getDeviceLocation() {
-        locationManager = (locationManager)getSystemService(Service.LOCATION_SERVICE);
+        //
+        locationManager = (LocationManager)getSystemService(Service.LOCATION_SERVICE);
         isGpsLocation = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
         isNetworkLocation = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
         if(!isGpsLocation && !isNetworkLocation){
             showSettingForLocation();
+            getLastLocation();
         }
         else {
             getFinalLocation();
+        }
+    }
+
+    private void getLastLocation() {
+        if(locationManager!=null){
+            try {
+                Criteria criteria = new Criteria();
+                String provider = locationManager.getBestProvider(criteria,false);
+                Location location = locationManager.getLastKnownLocation(provider);
+            }catch (SecurityException e){
+                e.printStackTrace();
+            }
         }
     }
 
@@ -92,16 +116,57 @@ public class MainActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
             case PREMISSION_CODE:
-                if
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getFinalLocation();
+                }
+                else {
+                    Toast.makeText(this, "Premission Failed", Toast.LENGTH_SHORT).show();
+                }
         }
     }
 
     private void getFinalLocation() {
+        try {
+            if(isGpsLocation){
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000*60*1,10,MainActivity.this);
+                if(locationManager!=null){
+                    loc=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    if(loc!=null){
+                        updateUi(loc);
+                    }
+                }
+            }
+            else if(isNetworkLocation){
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,1000*60*1,10,MainActivity.this);
+                if(locationManager!=null){
+                    loc=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                    if(loc!=null){
+                        updateUi(loc);
+                    }
+                }
+            }
+            else {
+                Toast.makeText(this,"Can't get Location",Toast.LENGTH_SHORT).show();
+            }
 
+        }catch (SecurityException e) {
+            Toast.makeText(this,"Can't get Location",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void updateUi(Location loc) {
+        progressDialog.dismiss();
+        if(loc.getLatitude()==0 && loc.getLongitude()==0){
+            getDeviceLocation();
+        }
+        else {
+            progressDialog.dismiss();
+            locationText.setText("Location : "+loc.getLatitude()+" , "+loc.getLongitude());
+        }
     }
 
     private void showSettingForLocation() {
-        AlertDialog.Builder al = new AlertDialog.Builder((MainActivity.this);
+        AlertDialog.Builder al = new AlertDialog.Builder(MainActivity.this);
         al.setTitle("Location Not Enable!");
         al.setMessage("Enable Location ?");
         al.setPositiveButton("YES", new DialogInterface.OnClickListener() {
@@ -116,7 +181,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(DialogInterface dialog, int which) {
                 dialog.cancel();
             }
-        })
+        });
+        al.show();
     }
 
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        updateUi(location);
+    }
 }
